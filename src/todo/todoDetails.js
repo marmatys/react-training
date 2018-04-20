195f5f88a32911d3ea3514600b71e9b963464b3c
Marcin Armatys <marcin.armatys@gmail.com>
2018-04-20 14:57:55 +0200

    Link from details to home^

--- src/todo/todoDetails.js
+++ src/todo/todoDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 
 class TodosDetails extends React.Component {
@@ -17,18 +18,19 @@ class TodosDetails extends React.Component {
         this.fetchData(this.props.match.params.id);
     }
 
-    fetchData(id) {
+    fetchData = (id) => {
         axios.get(`http://localhost:3001/todos/${id}`)
             .then(resp => resp.data)
             .then(data => this.setState({todo: {title: data.title, id: data.id}}))
             .catch(err => console.log(err));
-    }
+    };
 
     render() {
         return (
             <div>
-                <div>{this.state.todo.id}</div>
-                <div>{this.state.todo.title}</div>
+                <div>Id: {this.state.todo.id}</div>
+                <div>Title: {this.state.todo.title}</div>
+                <Link to="/">Back</Link>
             </div>
         );
     }
