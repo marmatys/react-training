704bbea241c5d1a0a2b2d721651757f4f8105058
Marcin Armatys <marcin.armatys@gmail.com>
2018-04-20 10:48:36 +0200

    Todos list - counter

--- src/todo/todos.js
+++ src/todo/todos.js
@@ -4,8 +4,12 @@ function Todos(props) {
     return (
         <div>
             <h1>Your todos from today</h1>
+            <div data-todos-count>You have {props.todos.length} todos</div>
         </div>
     );
+
 }
+
+Todos.defaultProps = {todos: []};
 
 export default Todos;
--- src/todo/todos.test.js
+++ src/todo/todos.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Todos from './todo';
+import Todos from './todos';
 import {mount} from 'enzyme';
 
 describe('Todo List', () => {
@@ -8,5 +8,27 @@ describe('Todo List', () => {
         const wrapper = mount(<Todos />);
 
         expect(wrapper.find('h1').text()).toEqual('Your todos from today');
+    });
+
+    it('renders todos count for 1 todo', () => {
+        const todos = [
+            {id: 1, title: 'Buy milk', completed: false}
+        ];
+
+        const wrapper = mount(<Todos todos={todos}/>);
+
+        expect(wrapper.find('[data-todos-count]').text()).toEqual('You have 1 todos')
+    });
+
+    it('renders todos count for 1 todo', () => {
+        const todos = [
+            {id: 1, title: 'Buy milk', completed: false},
+            {id: 2, title: 'Send PIT', completed: false},
+            {id: 3, title: 'Send email', completed: false},
+        ];
+
+        const wrapper = mount(<Todos todos={todos}/>);
+
+        expect(wrapper.find('[data-todos-count]').text()).toEqual('You have 3 todos')
     });
 });
