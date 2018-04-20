61e394c745cfbf842852b5608495f32ffa8e797b
Marcin Armatys <marcin.armatys@gmail.com>
2018-04-20 13:20:53 +0200

    Todos as Component with state

--- src/todo/todos.js
+++ src/todo/todos.js
@@ -2,16 +2,32 @@ import React from 'react';
 import TodosList from './todosList';
 import TodosNew from './todosNew';
 
-function Todos(props) {
-    return (
-        <div>
-            <h1>Your todos from today</h1>
-            <div data-todos-count>You have {props.todos.length} todos</div>
-            <TodosList todos={props.todos}/>
-            <TodosNew onNewTodo={(todo) => console.log('New todo', todo)}/>
-        </div>
-    );
+class Todos extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: props.todos
+        }
+    }
+
+    onNewTodo = (todo) => {
+        this.setState({
+            // todos: [...this.state.todos, _.extend(todo, {key: _.uniq()})]
+            todos: [...this.state.todos, todo]
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Your todos from today</h1>
+                <div data-todos-count>You have {this.state.todos.length} todos</div>
+                <TodosList todos={this.state.todos}/>
+                <TodosNew onNewTodo={this.onNewTodo}/>
+            </div>
+        );
+    }
 }
 
 Todos.defaultProps = {todos: []};
